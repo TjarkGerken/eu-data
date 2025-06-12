6800bf6c0c10e3957f836015283c6e03d92f0b30
yKingstone <yko@mobile-vision-technology.com>
2025-06-12 23:01:49 +0200

    add requirements and flood bar chart

--- eu_climate/main.py
+++ eu_climate/main.py
@@ -180,7 +180,7 @@ class RiskAssessment:
         flood_extents = hazard_layer.process_scenarios()
         
         # Create visualizations
-        # hazard_layer.visualize_hazard_assessment(flood_extents, save_plots=True)
+        hazard_layer.visualize_hazard_assessment(flood_extents, save_plots=True)
         
         # Export results
         hazard_layer.export_results(flood_extents)
