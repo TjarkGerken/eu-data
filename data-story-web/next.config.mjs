694bf65b94b0f62d10911ee6d5e84f029c78c120
Tjark Gerken <tjark.gerken@accenture.com>
2025-07-02 13:21:16 +0200

    fixes

--- data-story-web/next.config.mjs
+++ data-story-web/next.config.mjs
@@ -18,6 +18,20 @@ const nextConfig = {
       },
     ];
 
+    config.plugins.push({
+      apply(compiler) {
+        compiler.hooks.done.tap("LogBuildModulesPlugin", (stats) => {
+          const info = stats.toJson();
+          if (stats.hasErrors()) {
+            console.error("\n\n🚨 Build Errors:");
+            info.errors.forEach((e) => {
+              console.error(e.message || e);
+            });
+          }
+        });
+      },
+    });
+
     // Configure better-sqlite3 for server-side code
     if (isServer) {
       config.externals.push("better-sqlite3");
